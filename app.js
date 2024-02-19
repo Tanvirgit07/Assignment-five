f39893e97002bbd4ecb2758387d754f0861120b7
tanvir <tanvirgithub2024@gmail.com>
2024-02-19 14:51:57 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const allSeatButton = document.getElementsByClassName('btn_seat')
+let count = 0;
+let lessCount = 40;
+let totalTaka = 0;
+for(const buttonSeat of allSeatButton){
+    buttonSeat.addEventListener('click',function(event){
+        event.target.disabled = true;
+        event.target.style.backgroundColor = "#1DD100";
+        count = count + 1;
+        const seatNumber = document.getElementById('selected_seat')
+        seatNumber.innerText = count;
+        lessCount = lessCount - 1;
+        const selectedAfter = document.getElementById('selected_after')
+        selectedAfter.innerText = lessCount;
+        const buttonInnerText = event.target.innerText;
+        const addSeat = document.getElementById('total_seat')
+        const h1 = document.createElement('h1');
+        addSeat.appendChild(h1);
+        const seatPosition = 'Economoy'
+        const seatPrice = 500;
+        const p1 = document.createElement('p')
+        const p2 = document.createElement('p')
+        const p3 = document.createElement('p')
+        p1.innerText = buttonInnerText;
+        p2.innerText = seatPosition;
+        p3.innerText = seatPrice;
+
+        h1.appendChild(p1)
+        h1.appendChild(p2)
+        h1.appendChild(p3)
+
+        h1.classList.add('flex');
+        h1.classList.add('justify-between')
+        
+        totalTaka = totalTaka + 550;
+
+        const totalPrice = document.getElementById('total_tk')
+        totalPrice.innerText = totalTaka;
+        const grandTaka = document.getElementById('grane_tk');
+        grandTaka.innerText = totalTaka
+})
+
+
+}
